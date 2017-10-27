b0ef228d192e43830036ae89483d80bbe5717d1c
Lionel Villard <villard@us.ibm.com>
2017-10-27 17:32:59 -0400

    cleanup package.json and export models

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,6 @@
 export * from './HttpRequestParser';
 export * from './CurlRequestParser';
 export * from './httpClient';
+export * from './models/httpRequest';
+export * from './models/httpResponse';
+export * from './models/configurationSettings';
